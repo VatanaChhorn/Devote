489f6222b9899e7a435fad41c98f9f2ff5c2bb04
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2021-05-08 15:48:36 +0700

    2. Introduction to Core Data and understand the Xcode's Core Data template

---
+++ Devote/Utility/Constants .swift
@@ -0,0 +1,21 @@
+//
+//  Constants .swift
+//  Devote
+//
+//  Created by Vatana Chhorn on 08/05/2021.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX 
