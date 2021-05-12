34ccd3b7b19a2b00344fce025dcb718ff9150ff1
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2021-05-12 17:38:16 +0700

    Finished up the project

--- DevoteWidget/DevoteWidget.swift
+++ DevoteWidget/DevoteWidget.swift
@@ -12,15 +12,15 @@ struct Provider: TimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date())
     }
-
+    
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date())
         completion(entry)
     }
-
+    
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
-
+        
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
@@ -28,7 +28,7 @@ struct Provider: TimelineProvider {
             let entry = SimpleEntry(date: entryDate)
             entries.append(entry)
         }
-
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -39,29 +39,89 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct DevoteWidgetEntryView : View {
+    
     var entry: Provider.Entry
-
+    @Environment(\.widgetFamily) var widgetFamily
+    
+    var fontStyle : Font {
+        if widgetFamily == .systemSmall {
+            return .system(.footnote, design: .rounded)
+        } else {
+            return .system(.headline, design: .rounded)
+        }
+    }
+    
     var body: some View {
-        Text(entry.date, style: .time)
+        // Text(entry.date, style: .time)
+        
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                Image("rocket-small")
+                    .resizable()
+                    .scaledToFit()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(width: widgetFamily != .systemSmall ? 52 : 36,
+                           height: widgetFamily != .systemSmall ? 52 : 36)
+                    .offset(x: (geometry.size.width / 2) - 20,
+                            y: (geometry.size.height / -2) + 20)
+                    .padding(.top , widgetFamily != .systemSmall ? 32 : 12)
+                    
+                    .padding(.trailing,widgetFamily != .systemSmall ? 32 : 12)
+                
+                HStack {
+                    Text("Just Do It")
+                        .foregroundColor(.white)
+                        .font(fontStyle)
+                        .fontWeight(.bold)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(
+                            Color(red: 0, green: 0, blue: 0, opacity: 0.5)
+                                .blendMode(.overlay)
+                                .clipShape(Capsule())
+                    )
+                    
+                    if widgetFamily != .systemSmall {
+                        Spacer()
+                    }
+                }  //: HStack
+                .padding()
+                .offset(y: geometry.size.height / 2 - 24)
+                
+                
+            }  //: ZStack
+        }  //: GeometryReader
+        
     }
 }
 
 @main
 struct DevoteWidget: Widget {
     let kind: String = "DevoteWidget"
-
+    
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             DevoteWidgetEntryView(entry: entry)
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Devote Widget")
+        .description("This is an example widget for the personal task manager.")
     }
 }
 
 struct DevoteWidget_Previews: PreviewProvider {
     static var previews: some View {
-        DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
+        Group {
+            DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
+                .previewContext(WidgetPreviewContext(family: .systemSmall))
+            
+            DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
+                .previewContext(WidgetPreviewContext(family: .systemMedium))
+            
+            DevoteWidgetEntryView(entry: SimpleEntry(date: Date()))
+                .previewContext(WidgetPreviewContext(family: .systemLarge))
+        }
     }
 }
